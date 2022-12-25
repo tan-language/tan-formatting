61f10f66274182e00aa8cd72523414d1f9c644de
George Moschovitis <gmosx@reizu.com>
2022-12-25 20:25:14 +0200

    Cleanup

--- src/compact.rs
+++ src/compact.rs
@@ -2,36 +2,7 @@ use tan::expr::Expr;
 
 /// Formats an expression in compact form.
 pub fn format_compact(expr: &Expr) -> String {
-    match expr {
-        Expr::One => "()".to_owned(),
-        Expr::Bool(b) => b.to_string(),
-        Expr::Int(n) => n.to_string(),
-        Expr::Float(n) => n.to_string(),
-        Expr::Symbol(s) => s.clone(),
-        Expr::String(s) => s.clone(),
-        Expr::Do => "do".to_owned(),
-        // Expr::Do(terms) => {
-        //     format!(
-        //         "(do {})",
-        //         terms
-        //             .iter()
-        //             .map(|term| format!("{}", term.as_ref()))
-        //             .collect::<Vec<String>>()
-        //             .join(" ")
-        //     )
-        // }
-        Expr::List(terms) => {
-            format!(
-                "({})",
-                terms
-                    .iter()
-                    .map(|term| format!("{}", term.as_ref()))
-                    .collect::<Vec<String>>()
-                    .join(" ")
-            )
-        }
-        Expr::Func(..) => "#<func>".to_owned(),
-    }
+    format!("{expr}")
 }
 
 #[cfg(test)]
