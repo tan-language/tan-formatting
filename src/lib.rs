e369a64780dfbf0c44e4182119f301fdf0d64d3b
George Moschovitis <gmosx@reizu.com>
2023-06-17 12:58:09 +0300

    More work

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,22 @@
 pub mod pretty;
 mod util;
 
-use std::error::Error;
-
-use tan::{expr::Expr, range::Ranged};
+use tan::{
+    error::{Error, ErrorNote},
+    range::Position,
+};
 
 // #TODO reuse the Position from tan?
 // #TODO split into `format_expr`, `format_error`.
 // #TODO add special support for formatting multiple errors?
 
-// #TODO this is completely wrong, it skips comments, annotations, etc.
-/// Formats an expression in compact form.
-pub fn format_expr_compact(expr: impl AsRef<Expr>) -> String {
-    // #TODO even compact, strip whitespace aggressively!
-    format!("{}", expr.as_ref())
-}
+pub fn format_error_note_pretty(note: &ErrorNote, input: &str) -> String {
+    let Some(range) = &note.range else {
+        return format!("{}", note.text);
+    };
 
-/// Formats an expression in aestheticall pleasing form.
-/// This is the standard textual representation of expressions.
-pub fn format_expr_pretty(_expr: &Expr) -> String {
-    todo!()
-}
-
-// #TODO also format error without input.
-// #TODO implement this in ...Tan :)
-// #TODO format the error as symbolic expression.
-// #TODO format the error as JSON.
-// #TODO make more beautiful than Rust.
-// #TODO add as method to Ranged<E: Error>? e.g. `format_pretty`
-pub fn format_error_pretty<E: Error>(error: &Ranged<E>, input: &str, url: Option<&str>) -> String {
+    // #TODO do this once, outside of this function!
     let chars = input.chars();
-    let Ranged(error, span) = error;
 
     let mut index: usize = 0;
     let mut line = 0;
@@ -41,7 +27,7 @@ pub fn format_error_pretty<E: Error>(error: &Ranged<E>, input: &str, url: Option
         index += 1;
 
         if c == '\n' {
-            if index > span.start {
+            if index > range.start {
                 break;
             }
 
@@ -58,29 +44,124 @@ pub fn format_error_pretty<E: Error>(error: &Ranged<E>, input: &str, url: Option
 
     let line_space = " ".repeat(format!("{}", line + 1).len());
 
-    let len = span.len();
+    let len = range.len();
 
-    let indicator = if len == 1 {
-        "^--- near here".to_owned()
-    } else {
-        "^".repeat(len)
-    };
+    // let indicator = if len == 1 {
+    //     "^--- near here".to_owned()
+    // } else {
+    //     "^".repeat(len)
+    // };
 
-    let col = span.start - line_start;
+    // #TODO use `^` or `-` depending on note importance, like Rust.
+
+    let indicator = "^".repeat(len);
+
+    let col = range.start - line_start;
     let indicator_space = " ".repeat(col);
 
-    let url = url.unwrap_or("input");
-
     format!(
-        "{error}\n{}at {url}:{}:{}\n{}|\n{}| {}\n{}|{} {}",
-        line_space,
-        line + 1,
-        col + 1,
+        "{}|\n{}| {}\n{}|{} {} {}",
         line_space,
         line + 1,
         line_str,
         line_space,
         indicator_space,
         indicator,
+        note.text,
     )
 }
+
+// #TODO also format error without input.
+// #TODO implement this in ...Tan :)
+// #TODO format the error as symbolic expression.
+// #TODO format the error as JSON.
+// #TODO make more beautiful than Rust.
+// #TODO add as method to Ranged<E: Error>? e.g. `format_pretty`
+pub fn format_error_pretty(error: &Error, input: &str) -> String {
+    let Some(note) = error.notes.first() else {
+        return format!("{}\n at {}", error.kind(), error.file_url);
+    };
+
+    let prologue = if let Some(range) = &note.range {
+        let position = Position::from_range(&range, input);
+        format!(
+            "{}\n at {}:{}:{}",
+            error.kind(),
+            error.file_url,
+            position.line + 1,
+            position.col + 1,
+        )
+    } else {
+        format!("{}\n at {}", error.kind(), error.file_url)
+    };
+
+    let notes: Vec<String> = error
+        .notes
+        .iter()
+        .map(|note| format_error_note_pretty(note, input))
+        .collect();
+
+    format!("{prologue}\n{}", notes.join("\n"))
+}
+
+// #TODO also format error without input.
+// #TODO implement this in ...Tan :)
+// #TODO format the error as symbolic expression.
+// #TODO format the error as JSON.
+// #TODO make more beautiful than Rust.
+// #TODO add as method to Ranged<E: Error>? e.g. `format_pretty`
+// pub fn format_error_pretty_old<E: Error>(error: &E, input: &str, url: Option<&str>) -> String {
+//     let chars = input.chars();
+
+//     let mut index: usize = 0;
+//     let mut line = 0;
+//     let mut line_start: usize = 0;
+//     let mut line_str = String::new();
+
+//     for c in chars {
+//         index += 1;
+
+//         if c == '\n' {
+//             if index > error.range.start {
+//                 break;
+//             }
+
+//             line += 1;
+//             line_start = index;
+
+//             line_str.clear();
+
+//             continue;
+//         }
+
+//         line_str.push(c);
+//     }
+
+//     let line_space = " ".repeat(format!("{}", line + 1).len());
+
+//     let len = span.len();
+
+//     let indicator = if len == 1 {
+//         "^--- near here".to_owned()
+//     } else {
+//         "^".repeat(len)
+//     };
+
+//     let col = span.start - line_start;
+//     let indicator_space = " ".repeat(col);
+
+//     let url = url.unwrap_or("input");
+
+//     format!(
+//         "{error}\n{}at {url}:{}:{}\n{}|\n{}| {}\n{}|{} {}",
+//         line_space,
+//         line + 1,
+//         col + 1,
+//         line_space,
+//         line + 1,
+//         line_str,
+//         line_space,
+//         indicator_space,
+//         indicator,
+//     )
+// }
