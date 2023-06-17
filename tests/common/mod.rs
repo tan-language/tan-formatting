ea12d0944a9197f1ed5f691a143b2f5bc9dd9c36
George Moschovitis <gmosx@reizu.com>
2023-06-17 15:05:32 +0300

    Some fixes

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -3,20 +3,19 @@ use tan::api::{lex_string, parse_string_all};
 use tan::error::Error;
 use tan::expr::Expr;
 use tan::lexer::token::Token;
-use tan::range::Ranged;
 
 pub fn read_file(filename: &str) -> String {
     std::fs::read_to_string(format!("tests/fixtures/{filename}")).unwrap()
 }
 
 #[allow(dead_code)]
-pub fn lex_file(filename: &str) -> Result<Vec<Ranged<Token>>, Vec<Ranged<Error>>> {
+pub fn lex_file(filename: &str) -> Result<Vec<Token>, Vec<Error>> {
     let input = &read_file(filename);
     lex_string(input)
 }
 
 #[allow(dead_code)]
-pub fn parse_file(filename: &str) -> Result<Vec<Ann<Expr>>, Vec<Ranged<Error>>> {
+pub fn parse_file(filename: &str) -> Result<Vec<Ann<Expr>>, Vec<Error>> {
     let input = &read_file(filename);
     parse_string_all(input)
 }
