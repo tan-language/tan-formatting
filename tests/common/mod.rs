b9148862d03191741aa6cc1d89fea7462a923d75
Georgios Moschovitis <gmosx@reizu.com>
2024-06-27 19:45:41 +0300

    Use tan-analysis. #refactor

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -2,8 +2,7 @@ use tan::api::lex_string;
 use tan::error::Error;
 use tan::expr::Expr;
 use tan::lexer::token::Token;
-use tan::lexer::Lexer;
-use tan::parser::Parser;
+use tan_analysis::parsing::parse_string_for_analysis;
 
 pub fn read_file(filename: &str) -> String {
     std::fs::read_to_string(format!("tests/fixtures/{filename}")).unwrap()
@@ -18,19 +17,5 @@ pub fn lex_file(filename: &str) -> Result<Vec<Token>, Vec<Error>> {
 #[allow(dead_code)]
 pub fn parse_file(filename: &str) -> Result<Vec<Expr>, Vec<Error>> {
     let input = &read_file(filename);
-    parse_string_all(input)
-}
-
-// #todo Extract to util library (tan-analysis)?
-// Custom method that uses the analysis parser!
-pub fn parse_string_all(input: impl AsRef<str>) -> Result<Vec<Expr>, Vec<Error>> {
-    let input = input.as_ref();
-
-    let mut lexer = Lexer::new(input);
-    let tokens = lexer.lex()?;
-
-    let mut parser = Parser::for_analysis(&tokens);
-    let exprs = parser.parse()?;
-
-    Ok(exprs)
+    parse_string_for_analysis(input)
 }
